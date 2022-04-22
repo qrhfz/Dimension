205251ce983196871af4ef1a6d87133337c558de
Qori El-Hafizh <contact@qori.dev>
2022-04-22 19:44:16 +0700

    user test

--- test/models/user_test.dart
+++ test/models/user_test.dart
@@ -1,1 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hn_client/models/user.dart';
 
+import '../dummy_objects.dart';
+
+void main() {
+  test("Given Map object fromJson should return User object", () {
+    final User actual = User.fromJson(testJsonUser);
+    expect(actual, testUser);
+  });
+}
