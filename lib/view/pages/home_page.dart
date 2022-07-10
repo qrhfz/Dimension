5d671ecbc174dabff102d590509ebefdac7b875b
Qori El-Hafizh <contact@qori.dev>
2022-07-10 18:44:23 +0700

    responsive layout

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -26,56 +26,84 @@ class _HomePageState extends ConsumerState<HomePage> {
   Widget build(context) {
     final state = ref.watch(homeNotifierProvider);
     final notifier = ref.read(homeNotifierProvider.notifier);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(state.contentType.name),
-        actions: [
-          PopupMenuButton(
-            icon: const Icon(Icons.filter_list_rounded),
-            initialValue: state.contentType,
-            itemBuilder: (ctx) => const [
-              PopupMenuItem<HomeContentType>(
-                child: Text("Top"),
-                value: HomeContentType.top,
+
+    final types = [
+      HomeContentType.top,
+      HomeContentType.best,
+      HomeContentType.new_,
+      HomeContentType.ask,
+      HomeContentType.show,
+      HomeContentType.job
+    ];
+    final currentIndex = types.indexOf(state.contentType);
+
+    void onIndexChange(index) {
+      notifier.load(types[index]);
+    }
+
+    ;
+    return LayoutBuilder(builder: (context, constraint) {
+      if (constraint.maxWidth <= 800) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(state.contentType.name),
+          ),
+          body: Row(
+            children: [
+              MyNavRail(
+                types: types,
+                onTap: onIndexChange,
+                currentIndex: currentIndex,
               ),
-              PopupMenuItem<HomeContentType>(
-                child: Text("Best"),
-                value: HomeContentType.best,
-              ),
-              PopupMenuItem<HomeContentType>(
-                child: Text("New"),
-                value: HomeContentType.new_,
-              ),
-              PopupMenuItem<HomeContentType>(
-                child: Text("Ask"),
-                value: HomeContentType.ask,
-              ),
-              PopupMenuItem<HomeContentType>(
-                child: Text("Show"),
-                value: HomeContentType.show,
-              ),
-              PopupMenuItem<HomeContentType>(
-                child: Text("Job"),
-                value: HomeContentType.job,
+              Flexible(
+                child: state.when(
+                  loading: loading,
+                  data: data,
+                  error: error,
+                ),
               ),
             ],
-            onSelected: (HomeContentType type) {
-              notifier.load(type);
-            },
-          )
-        ],
-      ),
-      body: state.when(
-        loading: loading,
-        data: data,
-        error: error,
-      ),
-    );
+          ),
+          bottomNavigationBar: MyNavBar(
+            types: types,
+            currentIndex: currentIndex,
+            onTap: onIndexChange,
+          ),
+        );
+      }
+
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(state.contentType.name),
+        ),
+        body: Row(
+          children: [
+            MyNavRail(
+              types: types,
+              onTap: onIndexChange,
+              currentIndex: currentIndex,
+            ),
+            Flexible(
+              child: state.when(
+                loading: loading,
+                data: data,
+                error: error,
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: MyNavBar(
+          types: types,
+          currentIndex: currentIndex,
+          onTap: onIndexChange,
+        ),
+      );
+    });
   }
 
   Widget loading(_) => const Center(child: CircularProgressIndicator());
 
-  Widget data(_, List<int> ids) {
+  Widget data(HomeContentType type, List<int> ids) {
     return ListView.builder(
       itemCount: ids.length,
       itemBuilder: (ctx, index) {
@@ -87,5 +115,75 @@ class _HomePageState extends ConsumerState<HomePage> {
 
   Widget error(_, Failure failure) {
     return Center(child: Text(failure.message));
+  }
+}
+
+class MyNavBar extends StatelessWidget {
+  const MyNavBar({
+    Key? key,
+    required this.types,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  final List<HomeContentType> types;
+  final int currentIndex;
+
+  final Function(int) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      fixedColor: Colors.white,
+      unselectedItemColor: Colors.grey,
+      currentIndex: currentIndex,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Top"),
+        BottomNavigationBarItem(icon: Icon(Icons.trending_up), label: "Best"),
+        BottomNavigationBarItem(icon: Icon(Icons.new_releases), label: "New"),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.question_answer),
+          label: "Ask HN",
+        ),
+        BottomNavigationBarItem(icon: Icon(Icons.stars), label: "Show HN"),
+        BottomNavigationBarItem(icon: Icon(Icons.cases), label: "Jobs"),
+      ],
+      onTap: onTap,
+    );
+  }
+}
+
+class MyNavRail extends StatelessWidget {
+  const MyNavRail({
+    Key? key,
+    required this.currentIndex,
+    required this.types,
+    required this.onTap,
+  }) : super(key: key);
+
+  final List<HomeContentType> types;
+  final int currentIndex;
+  final Function(int) onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationRail(
+        onDestinationSelected: onTap,
+        destinations: const [
+          NavigationRailDestination(icon: Icon(Icons.home), label: Text("Top")),
+          NavigationRailDestination(
+              icon: Icon(Icons.trending_up), label: Text("Best")),
+          NavigationRailDestination(
+              icon: Icon(Icons.new_releases), label: Text("New")),
+          NavigationRailDestination(
+            icon: Icon(Icons.question_answer),
+            label: Text("Ask HN"),
+          ),
+          NavigationRailDestination(
+              icon: Icon(Icons.stars), label: Text("Show HN")),
+          NavigationRailDestination(
+              icon: Icon(Icons.cases), label: Text("Jobs")),
+        ],
+        selectedIndex: currentIndex);
   }
 }
