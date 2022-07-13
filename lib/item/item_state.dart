20e1f56054b5705d759cab6a3c99551b2b47c7ec
Qori El-Hafizh <contact@qori.dev>
2022-07-13 14:29:14 +0700

    use generic asyncstate

--- lib/item/item_state.dart
+++ lib/item/item_state.dart
@@ -1,11 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hn_client/common/asyncstate.dart';
 import 'package:hn_client/models/item.dart';
 
-part 'item_state.freezed.dart';
-
-@freezed
-class ItemState with _$ItemState {
-  const factory ItemState.loading() = _Loading;
-  const factory ItemState.data(Item item) = _Item;
-  const factory ItemState.error(String message) = _Error;
-}
+typedef ItemState = AsyncState<Item>;
