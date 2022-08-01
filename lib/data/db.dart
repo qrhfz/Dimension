6841a22db643b7aa1d946e0d11b82053f4997b02
Qori El-Hafizh <contact@qori.dev>
2022-08-01 14:23:46 +0700

    fix provider null assertion

--- lib/data/db.dart
+++ lib/data/db.dart
@@ -4,44 +4,41 @@ import 'package:path_provider/path_provider.dart';
 
 import '../models/visited_story.dart';
 
+final isarProvider = FutureProvider((ref) async {
+  final dir = await getApplicationSupportDirectory();
+  final isar = await Isar.open(
+    schemas: [VisitedStorySchema],
+    directory: dir.path,
+  );
+  return isar;
+});
+
 final dbProvider = Provider<DB>((ref) {
-  return DB();
+  final isar = ref.watch(isarProvider);
+  return DB(isar);
 });
 
 class DB {
-  Isar? _isar;
-
-  Isar get isar {
-    if (_isar == null) {
-      getIsar().then((value) => _isar = value);
-      return _isar!;
-    }
-    return _isar!;
+  DB(AsyncValue<Isar> isar) {
+    this.isar = isar.whenOrNull();
   }
 
-  Future<Isar> getIsar() async {
-    final dir = await getApplicationSupportDirectory();
-    final isar = await Isar.open(
-      schemas: [VisitedStorySchema],
-      directory: dir.path,
-    );
-    return isar;
-  }
-
-  Future<void> init() async {
-    _isar = await getIsar();
-  }
-
-  DB() {
-    init();
-  }
+  late final Isar? isar;
 
   Future<bool> isLinkVisited(int id) async {
+    final isar = this.isar;
+    if (isar == null) {
+      return false;
+    }
     final count = await isar.visitedStories.where().itemIdEqualTo(id).count();
     return count == 1;
   }
 
   Future<void> setLinkVisited(int id) async {
+    final isar = this.isar;
+    if (isar == null) {
+      return;
+    }
     final item = VisitedStory()..itemId = id;
     final visited = await isLinkVisited(id);
     if (visited) return;
